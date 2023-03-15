12816c05e04a0ad550b08888817656d8d0a509f2
carloscurcino <carlosdanielf541@gmail.com>
2023-03-15 14:09:52 -0300

    first commit

---
+++ CPU.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    unsigned int MBR;
+    unsigned short int MAR;
+    unsigned short int IBR;
+    unsigned char IR;
+    unsigned short int IMM;
+    unsigned short int PC;
+    unsigned char E, L, G;
+    unsigned char LR;
+    unsigned short int A;
+    unsigned short int B;
+    unsigned short int T;
+
+    unsigned char memoria[154];
+
+    return 0;
+}
