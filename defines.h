ee097bb817c8d0799547bdadc456e014db63958e
carloscurcino <carlosdanielf541@gmail.com>
2023-03-16 11:24:56 -0300

    mudando defines.h

--- defines.h
+++ defines.h
@@ -10,16 +10,16 @@
 #define or 0b01001;
 #define xor 0b01010;
 #define not 0b01011;
-#define jeMX 0b01100;
-#define jneMX 0b01101;
-#define jleMX 0b01111;
-#define jgMX 0b10000;
-#define jgeMX 0b10001;
-#define jmpMX 0b10010;
-#define ldaMX 0b10011; // Provavelmente é melhor tirar o M[X] e deixar só lda e ldb
-#define ldbMX 0b10100;
-#define staMX 0b10101;
-#define stbMX 0b10110;
+#define je 0b01100;
+#define jne 0b01101;
+#define jle 0b01111;
+#define jg 0b10000;
+#define jge 0b10001;
+#define jmp 0b10010;
+#define lda 0b10011; // Provavelmente é melhor tirar o M[X] e deixar só lda e ldb
+#define ldb 0b10100;
+#define sta 0b10101;
+#define stb 0b10110;
 #define ldrb 0b10111;
 #define movialimm 0b11000;
 #define moviahimm 0b11001;
