f93622315992b9790697fb4c06f612ab947577b3
carloscurcino <carlosdanielf541@gmail.com>
2023-03-15 23:16:42 -0300

    Adicionando o resto dos OP code ao defines.h

--- defines.h
+++ defines.h
@@ -12,3 +12,20 @@
 #define not 0b01011;
 #define jeMX 0b01100;
 #define jneMX 0b01101;
+#define jleMX 0b01111;
+#define jgMX 0b10000;
+#define jgeMX 0b10001;
+#define jmpMX 0b10010;
+#define ldaMX 0b10011; // Provavelmente é melhor tirar o M[X] e deixar só lda e ldb
+#define ldbMX 0b10100;
+#define staMX 0b10101;
+#define stbMX 0b10110;
+#define ldrb 0b10111;
+#define movialimm 0b11000;
+#define moviahimm 0b11001;
+#define addiaimm 0b11010;
+#define subiaimm 0b11011;
+#define muliaimm 0b11100;
+#define diviaimm 0b11101;
+#define lshimm 0b11110;
+#define rshimm 0b11111;
